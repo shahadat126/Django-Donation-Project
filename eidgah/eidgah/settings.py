eb7f38bb1ab662f6b63b0dcb3cf8f087187e46a8
shahadat126 <50002217+shahadat126@users.noreply.github.com>
2026-07-13 14:05:37 +0600

    Set DEBUG to False in settings.py
    Changed DEBUG setting to False for production.

--- eidgah/eidgah/settings.py
+++ eidgah/eidgah/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-we(ew=wcm2$&3au@kjr3m-x%mg%u0t4dw8#c-w-=@#n-p^48o-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

15ad2694e00733f580e5667a8752c3bec8220f6b
shahadat126 <mdshahadathossainzen@gmail.com>
2026-08-02 19:32:07 +0600

    Link update

--- App/urls.py
+++ App/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home,create_Donation,donation_list,delete,edit,Signup,Login,Logout,UserProfile
 
 urlpatterns = [
-    path('home/',home,name="home"),
+    path('',home,name="home"),
     path('creates/',create_Donation,name="create"),
     path('list/',donation_list,name="list"),
     path('edit/<int:id>/',edit,name="edit"),
